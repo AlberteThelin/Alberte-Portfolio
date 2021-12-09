3af76d9b8925ea8de4019cebe3d968e9159c6311
AlberteThelin <albe879d@edu.zealand.dk>
2021-12-09 12:11:30 +0100

    edit

---
+++ script.js
@@ -0,0 +1,7 @@
+const burger = document.getElementById('burger');
+const navUL = document.getElementById('navi-ul');
+
+
+burger.addEventListener('click', () => {
+navUL.classList.toggle('show');
+    });
