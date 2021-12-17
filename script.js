2133165590a5e9fd15e12df9380f904f299052e4
AlberteThelin <94894028+AlberteThelin@users.noreply.github.com>
2021-12-17 11:15:47 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const burger = document.getElementById('burger');
 const navUL = document.getElementById('navi-ul');
 
-
 burger.addEventListener('click', () => {
-navUL.classList.toggle('show');
-    });
+    navUL.classList.toggle('show');
+});
+
+/* Javascriptet til navigationsbaren har jeg fået fra Emil S. Nielsen*/
